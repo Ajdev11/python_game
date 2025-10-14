a4e0b01c5738749b55dd79324037dfd2546879c3
unknown <mosesogun11@gmail.com>
2025-10-14 18:57:18 +0200

    funct

--- test.py
+++ test.py
@@ -6,4 +6,36 @@ animals += ["pig", "goat"]
 animals.remove("dog")
 animals.insert(0, "ragnar")
 animals.pop()
-print(animals)
+# print(animals)
+
+
+
+
+# tuples, sets, dictionaries and list
+
+# list
+animals =  ["dog", "cat", "mouse", "bird", "snake", "turtle", "fish", "lizard", "frog", "toad"]
+# print(animals)
+
+# tuple
+animals_tuple = ("dog", "cat", "mouse", "bird", "snake", "turtle", "fish", "lizard", "frog", "toad")
+# print(animals_tuple)
+
+# set
+animals_set = {"dog", "cat", "mouse", "bird", "snake", "turtle", "fish", "lizard", "frog", "toad"}
+animals_set2 = {"dog", "cat"}
+mod = animals_set < animals_set2
+# print(mod)
+
+# dictionary
+names = {"first_name": "John", "last_name": "Doe", "age": 30, "city": "New York"}
+# print(names)
+
+
+# functions
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Moses")
+greet("John")
+greet("Jane")
