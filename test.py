a1326c17f8913842ff0939fcae2e37909bedf7fa
unknown <mosesogun11@gmail.com>
2025-10-15 18:33:31 +0200

    userAge

--- test.py
+++ test.py
@@ -33,9 +33,62 @@ names = {"first_name": "John", "last_name": "Doe", "age": 30, "city": "New York"
 
 
 # functions
-def greet(name):
-    print(f"Hello, {name}!")
+# def greet(name):
+#     print(f"Hello, {name}!")
+#     return name
 
-greet("Moses")
-greet("John")
-greet("Jane")
+# names_of_people = greet("Moses is a good boy")
+# names_of_people = greet("John is a good boy")
+# names_of_people = greet("Jane is a good girl")
+
+
+def name_age(name, age):
+    print(f"Hello, {name}! You are {age} years old.")
+    # return name, age
+
+# name_age("Moses", 20)
+# name_age("John", 21)
+# name_age("Jane", 22)
+
+# def change(value):
+#     value["name"] = "olaseni"
+
+# val = {"name":"dumdum"}
+# change(val)
+# print(val)
+
+
+
+# def age(years):
+#     if years >= 30:
+#         return "Moses is getting old"
+#     else:
+#         return "he is still young"
+
+# result = age(100)
+# print(f"i think {result} ")
+
+
+def userAge():
+   choice = int(input("Enter your age: "))
+   return choice
+age = userAge()
+if age >= 18:
+   print(f"Your age is {age}, you are an adult")
+elif age <= 18:
+    print(f"Your age is {age}. you are a minor")
+else:
+   print("enter a valid age")
+
+
+
+
+
+
+
+
+# the main purpose of a function is to avoid code duplication and the difference between a paramter
+#  and an argument is that a parameter is the variable in the function definition and an argument
+#  is the value passed to the function when it is called
+# return statement is used to return a value from a functio1n. if no return statement is used, the function will return None
+# return statement is used to exit a function
