125a816a69491fa6e3c5b58e430989376b10f556
unknown <mosesogun11@gmail.com>
2025-10-24 18:07:37 +0200

    class_self

--- blackJack.py
+++ blackJack.py
@@ -1,35 +1,50 @@
 import random
 
-
-cards = []
-suits = ["spades", "clubs", "Hearts", "Diamond"]
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
-
-
-def shuffle():
-    random.shuffle(cards)
-
-def deal(number):
-    card_dealt = []
-    for x in range(number):
-        card = cards.pop()
-        card_dealt.append(card)
-        return card_dealt
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return f"{self.rank['rank']} of {self.suit}"
 
 
-shuffle()
-card_dealt = deal(2)
-card = card_dealt[0]
-rank = card[1]
-if rank == "A":
-    value = 11
-elif rank == "J" or rank == "Q" or rank == "K":
-    value = 10
-else:
-    value = rank
 
 
-print(rank, value)
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suits = ["spades", "clubs", "Hearts", "Diamond"]
+        ranks = [{"rank": "A", "value":11},
+        {"rank": "2", "value":2},
+        {"rank": "3", "value":3},
+        {"rank": "4", "value":4},
+        {"rank": "5", "value":5},
+        {"rank": "6", "value":6},
+        {"rank": "7", "value":7},
+        {"rank": "8", "value":8},
+        {"rank": "9", "value":9},
+        {"rank": "10", "value":10},
+        {"rank": "J", "value":10},
+        {"rank": "Q", "value":10},
+        {"rank": "K", "value":10},
+        ]
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append([suit, rank])
+
+
+    def shuffle(self):
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)
+
+    def deal(self, number):
+        card_dealt = []
+        for x in range(number):
+            if len(self.cards) > 0:
+                card = self.cards.pop()
+                card_dealt.append(card)
+            return card_dealt
+
+
+card1 = Card("Hearts", {"rank": "J", "value": 10})
+print(card1)
