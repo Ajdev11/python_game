321c062ddd136c0a8781623bf13186ab3ff4cf20
unknown <mosesogun11@gmail.com>
2025-11-03 18:57:46 +0100

    add or remove

--- w3_school.py
+++ w3_school.py
@@ -226,3 +226,39 @@ print(list1)
 # A tuple is a collection which is ordered and unchangeable.
 thistuple = ("apple", "banana", "cherry")
 print(thistuple)
+
+# to print the number of items in the tuple
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))   
+
+# Convert the tuple into a list, add "orange", and convert it back into a tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+
+# add tuple to a tuple 
+
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
+
+# Tuples are unchangeable, so you cannot remove 
+# items from it, but you can use the same workaround as we used for changing and adding tuple items:
+
+
+# Convert the tuple into a list, remove "apple", and convert it back into a tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+# The del keyword can delete the tuple completely:
+
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple) #this will raise an error because the tuple no longer exists
