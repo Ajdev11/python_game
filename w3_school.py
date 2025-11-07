e29f647c93f6e63abf848301745824a7abed3f7f
unknown <mosesogun11@gmail.com>
2025-11-07 18:09:24 +0100

    match, pass, while loops

--- w3_school.py
+++ w3_school.py
@@ -688,3 +688,124 @@ has_discount_code = True
 
 if (age < 18 or age > 65) and not is_student or has_discount_code:
       print("Discount applies!")
+
+
+# nested if statements 
+
+# Checking multiple conditions with nesting:
+
+age = 25
+has_license = True
+
+if age >= 18:
+  if has_license:
+    print("You can drive")
+  else:
+    print("You need a license")
+else:
+  print("You are too young to drive")
+
+
+
+# if statements cannot be empty, but if you for some reason have an if statement with no content, put in the pass statement to avoid getting an error.
+
+a = 33
+b = 200
+
+if b > a:
+  pass
+
+
+# Match
+
+# Instead of writing many if..else statements, you can use the match statement.
+
+# The match statement selects one of many code blocks to be executed.
+
+# match expression:
+#   case x:
+#     code block
+#   case y:
+#     code block
+#   case z:
+#     code block
+
+
+
+day = 4
+match day:
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
+
+# Use the underscore character _ as the last case value if you want a code block to execute when there are not other matches:
+
+
+day = 4
+match day:
+  case 6:
+    print("Today is Saturday")
+  case 7:
+    print("Today is Sunday")
+  case _:
+    print("Looking forward to the Weekend")
+
+
+# or we can also have somethinmg like this 
+
+day = 4
+match day:
+  case 1 | 2 | 3 | 4 | 5:
+    print("Today is a weekday")
+  case 6 | 7:
+    print("I love weekends!")
+
+
+# You can add if statements in the case evaluation as an extra condition-check:
+
+month = 5
+day = 4
+match day:
+  case 1 | 2 | 3 | 4 | 5 if month == 4:
+    print("A weekday in April")
+  case 1 | 2 | 3 | 4 | 5 if month == 5:
+    print("A weekday in May")
+  case _:
+    print("No match")
+
+
+# Python Loops
+# Python has two primitive loop commands:
+
+# while loops
+# for loops
+# The while Loop
+# With the while loop we can execute a set of statements as long as a condition is true.
+
+
+i = 1
+
+while i < 10:
+    print(i)
+    i += 1
+
+
+# exact the loops when it gets to 3
+
+i = 1
+while i < 6:
+    print(f"the value are {i}")
+    if i == 3:
+        break
+    i += 1
