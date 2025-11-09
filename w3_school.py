bda14f32400a68cf079bcf19fd1bcc169cdab0b8
unknown <mosesogun11@gmail.com>
2025-11-09 10:17:31 +0100

    arguemnt

--- w3_school.py
+++ w3_school.py
@@ -887,3 +887,25 @@ def salutation():
 
 greets = salutation()
 print(greets)
+
+
+# Arguments are specified after the function name, inside the parentheses. You can add as many arguments as you want, just separate them with a comma.
+
+# The following example has a function with one argument (fname). When the function is called, we pass along a first name, which is used inside the function to print the full name:
+
+
+def funct_name(fname): 
+    print(fname+ " is my name")
+    return
+
+funct_name("Moses")
+funct_name("Anjorin")
+
+# fname is the parameter passed
+# Moses and Anjorin is the argument 
+
+# From a function's perspective:
+
+# A parameter is the variable listed inside the parentheses in the function definition.
+
+# An argument is the actual value that is sent to the function when it is called.
